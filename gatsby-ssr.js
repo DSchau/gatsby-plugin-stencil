07adb2df0965310e318d932165a4426cf635e7d3
Dustin Schau <dustinschau@gmail.com>
2019-02-26 11:55:18 -0600

    chore: run prettier

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,24 +1,29 @@
-const stencil = require(`@stencil/core/server`)
-const { renderToString } = require(`react-dom/server`)
+const stencil = require(`@stencil/core/server`);
+const { renderToString } = require(`react-dom/server`);
 
-let renderer
+let renderer;
 
-exports.replaceRenderer = async function replaceRenderer({ bodyComponent, replaceBodyHTMLString }) {
+exports.replaceRenderer = async function replaceRenderer({
+  bodyComponent,
+  replaceBodyHTMLString,
+}) {
   // re-using the renderer here, you may not need to if this is cheap, or you don't want to re-use
   if (!renderer) {
     // no idea where this config should come from?
     // also needed to create nested www/ structure to get this to not fail out
-    renderer = new stencil.Renderer(stencil.loadConfig({
-      rootDir: process.cwd()
-    })) 
+    renderer = new stencil.Renderer(
+      stencil.loadConfig({
+        rootDir: process.cwd(),
+      })
+    );
   }
 
   // you may not even need document?
   // I'm not really sure how this works to be honest!
   // if you do, you can just renderToString, and then use domino (or whatever!) because we're invoking Node APIs at build time
   const { html } = await renderer.hydrateToString({
-    html: renderToString(bodyComponent)
-  })
+    html: renderToString(bodyComponent),
+  });
 
-  replaceBodyHTMLString(html)
-}
+  replaceBodyHTMLString(html);
+};
